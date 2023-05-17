a5cccd7273c0d67ee77cb082d162aacc21a15633
walotta <walotta@126.com>
2023-05-17 18:30:49 +0000

    udp download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,9 @@
+tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/CoLearn-Dev/colink-playbook-dev/releases/latest" | jq -r '.tag_name')
+
 if [ "$(uname)" == "Linux" ]; then
-  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/v0.1.0/colink-playbook-linux-x86_64
+  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/${tag}/colink-playbook-linux-x86_64
 elif [ "$(uname)" == "Darwin" ]; then
-  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/v0.1.0/colink-playbook-macos-x86_64
+  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/${tag}/colink-playbook-macos-x86_64
 else
   echo "Unsupported operating system" >&2
   exit 1
