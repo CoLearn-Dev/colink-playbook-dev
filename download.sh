10b3c05693e5af7c953ce1b684b67770da9aaa64
walotta <walotta@126.com>
2023-05-17 18:59:17 +0000

    fix bug in download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,10 @@
 tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/CoLearn-Dev/colink-playbook-dev/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+download_url=https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/${tag}
+echo ${download_url}
 if [ "$(uname)" == "Linux" ]; then
-  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/${tag}/colink-playbook-linux-x86_64
+  wget -O colink-playbook ${download_url}/colink-playbook-linux-x86_64
 elif [ "$(uname)" == "Darwin" ]; then
-  curl -o colink-playbook https://github.com/CoLearn-Dev/colink-playbook-dev/releases/download/${tag}/colink-playbook-macos-x86_64
+  wget -O colink-playbook ${download_url}/colink-playbook-macos-x86_64
 else
   echo "Unsupported operating system" >&2
   exit 1
