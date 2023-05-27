0817e7a2ced74678b37aa609284e13eeb4261279
littleRound <xiaoyuanliu@berkeley.edu>
2023-05-27 13:17:14 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
         String,
         Box<dyn colink::ProtocolEntry + Send + Sync>,
     > = std::collections::HashMap::new();
-    let toml_str = fs::read_to_string(config).unwrap();
-    let protocol_spec_vec = parse_spec_from_toml(&toml_str).unwrap();
+    let toml_str = fs::read_to_string(config).unwrap();  // provide some robust feedback to users here? It's a common glitch for users to put the file in the wrong place / use the wrong env var
+    let protocol_spec_vec = parse_spec_from_toml(&toml_str).unwrap();  // similarly, here, it's commmon for users to write a config with glitches, it would be more helpful to reflect the err clearly & friendly
     for protocol_spec in protocol_spec_vec {
         for role in protocol_spec.roles {
             let name = protocol_spec.protocol_name.clone() + ":" + role.name.as_str();
