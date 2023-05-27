e04dc78aa4f2ac0613d7ec4c2b271373ea3321c8
littleRound <xiaoyuanliu@berkeley.edu>
2023-05-27 13:12:38 -0700

    Update helper.rs

--- src/helper.rs
+++ src/helper.rs
@@ -1,15 +1,16 @@
+// utility?
 use std::env;
 
 use regex::Regex;
 
-pub fn replace_str(
-    to_replace: &str,
-    values_table: std::collections::HashMap<String, String>,
+pub fn replace_str(  // render template? ( I think we've discussed this?
+    to_replace: &str,  // to_be_replaced? (I actually prefer simply `s`, which does not cause any ambiguity)
+    values_table: std::collections::HashMap<String, String>,  // `assignments`?
 ) -> Result<String, Box<dyn std::error::Error + Send + Sync + 'static>> {
     let mut path = String::new();
     let mut i = 0;
     while i < to_replace.len() {
-        match to_replace[i..].find("{{") {
+        match to_replace[i..].find("{{") { // why using regex below, but here do it manually?
             Some(start) => {
                 path.push_str(&to_replace[i..i + start]);
                 let end = to_replace[i + start + 2..].find("}}").unwrap() + i + start + 2;
@@ -20,7 +21,7 @@ pub fn replace_str(
                         let substring_end = var_string.find(']').unwrap();
                         let var_name = &var_string[..substring_start];
                         let indexes = &var_string[substring_start + 1..substring_end];
-                        let values = values_table.get(var_name).unwrap();
+                        let values = values_table.get(var_name).unwrap();  // always try to avoid naming like `values`
                         let values = values.chars().collect::<Vec<char>>();
                         if indexes.is_empty() {
                             path.push_str(&values.iter().collect::<String>());
